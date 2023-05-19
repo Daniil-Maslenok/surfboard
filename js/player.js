ddf220b035b362339e0dc912bc3fdf01a60fe4d8
Daniil <126064471+Daniil-Maslenok@users.noreply.github.com>
2023-05-19 18:54:47 +0300

    Внедрена новые секции и функционал, реализован OnePageScroll
    В проект добавлены секции с видео и картами, доделан footer и всплывающее меню с параметрами продукции, а также добавлен функционал горизонтального аккордеона в секции "Меню"

---
+++ js/player.js
@@ -0,0 +1,34 @@
+let player;
+let windowHeight = $(window).width();
+const player__elem = $(".player__elem");
+
+console.log(windowHeight);
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('yt-player', {
+    height: '400',
+    width: '670',
+    videoId: 'q2a-wN6kKl4',
+    events: {
+      //   'onReady': onPlayerReady,
+      //   'onStateChange': onPlayerStateChange
+    },
+    playerVars: {
+      modestbranding: 1,
+      showinfo: 0,
+      rel: 0,
+    }
+  });
+}
+
+if (windowHeight <= 747) {
+
+  player__elem.css("width", "480px");
+  player__elem.css("height", "350px");
+
+}
+
+if (windowHeight <= 480) {
+  player__elem.css("width", "320px");
+  player__elem.css("height", "200px");
+}
